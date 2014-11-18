c90cf6a16006f3ee5f0a1e857c279bb812355113
Eyal Arubas <eyalarubas@gmail.com>
2014-11-18 20:10:33 +0800

    add a pretty tree reporter

---
+++ demo/demo.js
@@ -0,0 +1,2 @@
+require('./demo_part1');
+require('./demo_part2');
--- demo/demo_part2.js
+++ demo/demo_part2.js
@@ -1,38 +1,6 @@
-var suite = require('./');
+var suite = require('../lib/reporters/tree');
 
-suite("My application", function(test, suite){
-
-    test("basic function 1", function(t){
-        t.assert("foo" === "bar", "foo === bar");
-    }).todo("will land in version 2.3.2");
-
-    test("basic function 2", function(t){
-        setTimeout(function(){
-            t.ok();
-        }, 100);
-    });
-
-    test("basic function 3", function(t){
-        throw Error("Ooops");
-    });
-
-    test("basic function 4", function(){
-        // all good
-    });
-
-    suite("login", function(test){
-
-        test("with incorrect password", function(t){
-            setTimeout(function(){
-                t.notOk("login should've failed");
-            }, 300);
-        }).skip("logins server is down");
-
-        test("with correct password", function(t){
-            t.notOk("login should've succeeded");
-        });
-
-    });
+suite("My application - Part 2", function(test, suite){
 
     suite("shopping cart", function(test, suite){
 
@@ -40,7 +8,7 @@ suite("My application", function(test, suite){
             setTimeout(function(){
                 t.ok();
             }, 700);
-        });
+        }).todo();
 
         suite("items", function(test, suite){
 
